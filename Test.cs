50792348d91567c50fd3c674194c02b088b69309
MOON0832 <59637978+MOON0832@users.noreply.github.com>
2021-11-24 02:21:12 +0900

    Mesh renderer study

--- Test.cs
+++ Test.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    private BoxCollider col;
+    [SerializeField] //직접 inspector 창에서 넣을 수 있도록
+    private Material red_Mat;
+    [SerializeField]
+    private Material green_Mat;
+
+    private MeshRenderer mesh;
     
 
     void Start() {
 
-        col = GetComponent<BoxCollider>(); // boxcollider를 가져와서 col에 넣어준다. col변수로 큐브의 box colider를 통제할 수 있게 됨.
+        mesh = GetComponent<MeshRenderer>();
 
     }
 
 
-    private void OnTriggerStay(Collider other)
-    { //머물고 있으면 1초에 몇십번씩 계속 실행됨
-        other.transform.position += new Vector3(0, 0, 0.1f); //머물고 있으면, 상대방의 position을 계속 더해줌. (계속 0.01씩 이동)
-    }
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) //마우스 좌클릭을 하고 있으면
+        {
+            mesh.material = green_Mat; //초록색으로 바꿔주고
+        }
+        else //좌클릭을 하고 있지 않으면 
+        {
+            mesh.material = red_Mat; //빨간색으로 바꿔준다.
+        }
 
-    private void OnTriggerExit(Collider other)
-    { //빠져나간 순간, position을 하늘로 올려본다. 
-        other.transform.position += new Vector3(0, 2, 0); //머물고 있으면, 상대방의 position을 계속 더해줌. (계속 0.01씩 이동)
     }
 }
